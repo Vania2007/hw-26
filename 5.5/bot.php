eb4c7afb917e76d5b5adf18b868578d358ebfda2
Vania2007 <momotis2007@gmail.com>
2024-12-10 21:47:18 +0200

    new_version_bot

--- 5.5/bot.php
+++ 5.5/bot.php
@@ -1,10 +1,22 @@
 <?php
-include "./credits.php";
+include "token.php";
+$data = file_get_contents('php://input');
+$data = json_decode($data, true);
+file_put_contents(__DIR__ . '/message.txt', print_r($data, true));
+if (empty($data['message']['chat']['id'])) {
+    exit();
+}
 
-function sendMessage($response)
+function sendTelegram($method, $response)
 {
-    $url = "https://api.telegram.org/bot" . BOT_TOKEN . "/sendMessage?chat_id=" . $chat_id . "&text=" . urlencode($response);
-    file_get_contents($url);
+    $ch = curl_init('https://api.telegram.org/bot' . TOKEN . '/' . $method);
+    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $response);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_HEADER, false);
+    $res = curl_exec($ch);
+    curl_close($ch);
+    return $res;
 }
 
 function getWeather($city)
@@ -14,39 +26,49 @@ function getWeather($city)
     return json_decode($response, true);
 }
 
-$update = json_decode(file_get_contents("php://input"), true);
-$chat_id = $update['message']['chat']['id'];
-$message = $update['message']['text'];
+if (!empty($data['message']['text'])) {
+    $text = $data['message']['text'];
+    $chat_id = $data['message']['chat']['id'];
 
-if (preg_match('/\/weather (.+)/', $message, $matches)) {
-    $city = $matches[1];
-    $weather = getWeather($city);
+    if (preg_match('/\/weather (.+)/', $text, $matches)) {
+        $city = $matches[1];
+        $weather = getWeather($city);
 
-    if (isset($weather['main']['temp'])) {
-        $temp = $weather['main']['temp'];
-        $response = "У місті $city температура: $temp °C.";
-    } else {
-        $response = "Не вдалося знайти погоду для міста $city.";
+        if (isset($weather['main']['temp'])) {
+            $temp = $weather['main']['temp'];
+            $response = "У місті $city температура: $temp °C.";
+        } else {
+            $response = "Не вдалося знайти погоду для міста $city.";
+        }
+        sendTelegram('sendMessage', array('chat_id' => $chat_id, 'text' => $response));
+        exit();
     }
-} elseif (preg_match('/\/programming/', $message)) {
-    $response = "Я можу допомогти з такими мовами програмування: PHP, JavaScript, Java, C#, Python.\n";
-    $response .= "Якщо вас цікавить конкретна мова, просто запитайте про неї, наприклад:\n";
-    $response .= "/php - інформація про PHP\n";
-    $response .= "/javascript - інформація про JavaScript\n";
-    $response .= "/java - інформація про Java\n";
-    $response .= "/csharp - інформація про C#\n";
-    $response .= "/python - інформація про Python";
-} elseif (preg_match('/\/php/', $message)) {
-    $response = "PHP - це серверна мова програмування, яка широко використовується для веб-розробки. Вона дозволяє створювати динамічні веб-сторінки.";
-} elseif (preg_match('/\/javascript/', $message)) {
-    $response = "JavaScript - це мова програмування, яка використовується для створення інтерактивних елементів на веб-сторінках. Вона виконується на стороні клієнта.";
-} elseif (preg_match('/\/java/', $message)) {
-    $response = "Java - це об'єктно-орієнтована мова програмування, яка використовується для створення різноманітних додатків, від мобільних до веб-додатків.";
-} elseif (preg_match('/\/csharp/', $message)) {
-    $response = "C# - це мова програмування, розроблена Microsoft, яка використовується для створення різноманітних додатків, зокрема для Windows.";
-} elseif (preg_match('/\/python/', $message)) {
-    $response = "Python - це високорівнева мова програмування, яка відома своєю простотою та читабельністю. Вона використовується в науці про дані, веб-розробці та автоматизації.";
-} else {
-    $response = "Вітаю! Використовуйте /weather [місто] для отримання погоди або /programming для отримання інформації про мови програмування.";
+
+    if (preg_match('/\/(php|javascript|python|java|csharp)/i', $text, $matches)) {
+        $language = strtolower($matches[1]);
+        $info = '';
+
+        switch ($language) {
+            case 'php':
+                $info = "PHP - це серверна мова програмування, яка використовується для створення веб-додатків.";
+                break;
+            case 'javascript':
+                $info = "JavaScript - це мова програмування, яка дозволяє створювати інтерактивні елементи на веб-сторінках.";
+                break;
+            case 'python':
+                $info = "Python - це універсальна мова програмування, відома своєю простотою та читабельністю.";
+                break;
+            case 'java':
+                $info = "Java - це об'єктно-орієнтована мова програмування, яка використовується для створення платформо-незалежних додатків.";
+                break;
+            case 'csharp':
+                $info = "C# - це мова програмування, розроблена Microsoft, яка використовується для створення різноманітних додатків, зокрема для Windows.";
+                break;
+            default:
+                $info = "Не вдалося знайти інформацію про цю мову програмування.";
+        }
+
+        sendTelegram('sendMessage', array('chat_id' => $chat_id, 'text' => $info));
+        exit();
+    }
 }
-sendMessage($response);
